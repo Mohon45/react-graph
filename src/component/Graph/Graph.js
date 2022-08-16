ff7587143763fb6a6e7081ef33896c511cd29c2e
Md: Mohon <mdmohon6145@gmail.com>
2022-08-17 03:34:29 +0600

    comment out

--- src/component/Graph/Graph.js
+++ src/component/Graph/Graph.js
@@ -69,25 +69,6 @@ const data = [
   },
 ];
 
-// const renderCustomizedLabel = (props) => {
-//   const { x, y, width, value } = props;
-//   const radius = 10;
-
-//   return (
-//     <g>
-//       <circle cx={x + width / 2} cy={y - radius} r={radius} fill="#8884d8" />
-//       {/* <text
-//         x={x + width / 2}
-//         y={y - radius}
-//         fill="#fff"
-//         textAnchor="middle"
-//         dominantBaseline="middle"
-//       >
-//         {value.split(" ")[1]}
-//       </text> */}
-//     </g>
-//   );
-// };
 
 const Graph = () => {
   return (
